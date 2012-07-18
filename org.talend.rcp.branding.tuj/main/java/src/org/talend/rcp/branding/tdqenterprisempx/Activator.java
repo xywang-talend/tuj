2360a661f3a8134c8c05b4f2d9687c8269afeb5c
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-18 05:33:30 +0000

    Modification: finish the changes of rebranding for tujs
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@87653 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp.branding.tuj/main/java/src/org/talend/rcp/branding/tdqenterprisempx/Activator.java
+++ org.talend.rcp.branding.tuj/main/java/src/org/talend/rcp/branding/tdqenterprisempx/Activator.java
@@ -3,11 +3,10 @@ package org.talend.rcp.branding.tdqenterprisempx;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "org.talend.rcp.branding.tdqenterprisempx";
+    public static final String PLUGIN_ID = "org.talend.rcp.branding.tuj";
 
     // The shared instance
     private static Activator plugin;
@@ -24,6 +23,7 @@ public class Activator extends AbstractUIPlugin {
      * 
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
      */
+    @Override
     public void start(BundleContext context) throws Exception {
         super.start(context);
     }
@@ -33,6 +33,7 @@ public class Activator extends AbstractUIPlugin {
      * 
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
      */
+    @Override
     public void stop(BundleContext context) throws Exception {
         plugin = null;
         super.stop(context);
